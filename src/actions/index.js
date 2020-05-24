9d413ec37ed8e6959aba6d736c9cc23791183332
david11yf29 <david11yf29@gmail.com>
2020-05-24 10:26:15 +0800

    updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-    return function(dispatch, getState) {
+    return async function(dispatch, getState) {
 
-        const promise = jsonPlaceholder.get('/posts');
+        const response = await jsonPlaceholder.get('/posts');
 
-        return {
+        // Action 內部先 dispatch action
+        dispatch({
             type: 'FETCH_POSTS',
-            payload: promise
-        }
+            payload: response
+        })
     }
 }
